7517ccc4f5f1abaf85de613f6ec530804ef3b079
Veda <veda.balte@sitechhs.com>
2019-11-11 12:41:02 -0500

    ok

--- parallax-template/js/app.js
+++ parallax-template/js/app.js
@@ -51,27 +51,6 @@ const DOMStrings = {
       }
     });
   }
-
-  var bar=$('#progress_bar');
-var percentage=parseInt($('#progress_percentage').html());
-
-function stopProgress(){
-  clearInterval(progress);
-}
-
-var progress= setInterval(function(){
-  percentage++;
-  if (percentage<=100){
-    $('#progress_percentage').html(percentage+'%');
-    if (percentage>10) {
-      bar.css('width',percentage+'%');
-      console.log(percentage);
-    }
-  }
-  else {
-    stopProgress()
-  }
-},80);
   
   // document.getElementById("pic-submit").addEventListener("submit", function (e) {
 
